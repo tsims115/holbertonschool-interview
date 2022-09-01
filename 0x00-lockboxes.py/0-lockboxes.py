75454d46fe91297856302fab369cd18b7b313018
Trenton Sims <tsims115@yahoo.com>
2022-08-31 20:08:44 -0500

    adding task 0

---
+++ 0x00-lockboxes.py/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Contains canUnlockAll function """
+
+
+def canUnlockAll(boxes):
+    """checks to see if it can unlock all boxes"""
+    key_list = []
+    unlocked_boxes = []
+    n = len(boxes)
+    count = 0
+    i = 0
+    flag = 0
+    while i < n:
+        if ((i == 0) or (i in key_list)) and (i not in unlocked_boxes):
+            for key in boxes[i]:
+                key_list.append(key)
+            unlocked_boxes.append(i)
+            count += 1
+            flag = 1
+        if i == n - 1:
+            if flag == 1:
+                flag = 0
+            else:
+                return False
+            if count != n:
+                i = 0
+                continue
+        if count == n:
+            return True
+        i += 1
