e84ca68d4d22e2306b498f121bd28ba642334066
tsims115 <tsims115@yahoo.com>
2022-10-10 22:14:43 -0700

    Adding task 0

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -8,6 +8,6 @@
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    printf("%d", grid1[0][0]);
-    printf("%d", grid2[0][0]);
+	printf("%d", grid1[0][0]);
+	printf("%d", grid2[0][0]);
 }
