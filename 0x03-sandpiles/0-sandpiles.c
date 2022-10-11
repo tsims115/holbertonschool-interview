0a12da1b1085a742f52ff65decb5d09709f44c22
tsims115 <tsims115@yahoo.com>
2022-10-10 22:09:44 -0700

    Adding task 0

---
+++ 0x03-sandpiles/0-sandpiles.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * sandpiles_sum - adds together two sandpiles
+ *
+ * @grid1: first grid to add
+ * @grid2: second grid to add
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    printf("Hello World");
+}
