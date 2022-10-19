0fbabadaaf042579cbd4c4cc0513068ffd96a6a0
tsims115 <tsims115@yahoo.com>
2022-10-19 05:35:37 -0700

    Cleaning up code

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -8,25 +8,21 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t* cur_node = *head;
+	listint_t* cur = NULL;
 	int *array = NULL;
 	int size = 0;
 	int i;
 
-	if (head == NULL)
-		return (1);
-	while (cur_node != NULL)
-	{
-		cur_node = cur_node->next;
+	for (cur = *head; cur != NULL; cur = cur->next)
 		size++;
-	}
-	cur_node = *head;
+	cur = *head;
 	array = malloc(size * sizeof(int));
-	for (i = 0; cur_node != NULL; cur_node = cur_node->next)
-		array[i++] = cur_node->n;
+	for (i = 0; cur != NULL; cur = cur->next)
+		array[i++] = cur->n;
 	for (i = 0; i < size; i++)
 		if (array[i] != array[size - 1 - i])
 			return (0);
+	free(array);
 	return (1);
 	
 }
