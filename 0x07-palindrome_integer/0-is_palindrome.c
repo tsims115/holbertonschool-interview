90182587da7d5920e416fae4757142c5f0b9cbdc
tsims115 <tsims115@yahoo.com>
2022-10-27 12:09:23 -0700

    Adding task 0 changes

--- 0x07-palindrome_integer/0-is_palindrome.c
+++ 0x07-palindrome_integer/0-is_palindrome.c
@@ -12,22 +12,19 @@ int is_palindrome(unsigned long n)
 {
 	unsigned long test;
 	int size, i;
-	int *array;
+	unsigned long array[20];
 
 	size = 0;
 	for (test = n; test >= 10; test /= 10)
 		size++;
-	array = malloc(size * sizeof(int));
+	size++;
 	i = 0;
-	for (test = n; test >= 10; test /= 10)
+	for (test = n; i < size; test /= 10)
 	{
-		array[i++] = test % 10;
-		printf("%lu, ", (test % 10));
+		array[i] = test % 10;
+		printf("%lu ", array[i]);
+		i++;
 	}
-	printf("\n");
-	printf("I am here\n");
-	for (i = 0; i < size; i++)
-		printf("%u, ", array[i]);
 	printf("\n");
 	return (0);
 }
