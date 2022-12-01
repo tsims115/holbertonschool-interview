7a13ecb69a60ec5e12b0438356280717ee8fa0b4
tsims115 <tsims115@yahoo.com>
2022-12-01 21:44:10 +0000

    Adding task file

--- 0x09-slide_line/slide_line.h
+++ 0x09-slide_line/slide_line.h
@@ -3,18 +3,6 @@
 
 int slide_line(int *line, size_t size, int direction);
 
-void rvs(int *line, size_t size)
-{
-	int i, j, tmp;
-	j = (int)size - 1;
-	for (i = 0; i < (int)size / 2; i++)
-	{
-		tmp = line[i];
-		line[i] = line[j];
-		line[j] = tmp;
-		j--;
-	}
-}
 
 #define SLIDE_LEFT 1
 #define SLIDE_RIGHT 0
