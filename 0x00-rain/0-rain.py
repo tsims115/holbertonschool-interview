b4aa44ed0b723968b1b7985636715e954ec329a1
Trenton Sims <tsims115@yahoo.com>
2023-01-10 12:56:40 -0600

    Adding task 0

---
+++ 0x00-rain/0-rain.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def rain(walls):
+    last_wall = 0
+    wall_spaces = 0
+    total = 0
+    for wall in walls:
+        if wall > 0:
+            if wall > last_wall:
+                total += last_wall * wall_spaces
+            if last_wall > wall:
+                total += wall * wall_spaces
+            wall_spaces = 0
+            last_wall = wall
+        else:
+            wall_spaces += 1
+    return total
