7a007efd2e2c30bc16adf629f70cb9451abf124a
tsims115 <72327718+tsims115@users.noreply.github.com>
2023-03-09 10:12:49 -0600

    Adding changes

---
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,11 @@
+def island_perimeter(grid):
+    """Island_permiter"""
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if i == 0:
+                if grid[i][j] == 1:
+                    count += 4
+            if grid[i][j] == 1:
+                count += 2
+    return count + 2
